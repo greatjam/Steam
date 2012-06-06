924d85a262e519b16034caad7c00ed2fa11835bd
sprhawk <hongbo@yang.me>
2012-06-07 04:07:12 +0800

    修改了版本文本

--- YPlayer/Steam/Steam.m
+++ YPlayer/Steam/Steam.m
@@ -16,7 +16,7 @@
 //GNU Lesser General Public License for more details.
 //
 //You should have received a copy of the GNU Lesser General Public 
-//License along with YToolkit.  If not, see <http://www.gnu.org/licenses/>.
+//License along with Steam.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "Steam.h"
